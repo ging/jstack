2f4494db1b1bbc75a318b56a977e5529621dfd11
iljdam <irenatr@gmail.com>
2013-10-28 15:00:02 +0100

    Neutron API library added

---
+++ src/JSTACK.Neutron.js
@@ -0,0 +1,631 @@
+/*
+The MIT License
+
+Copyright (c) 2013 Universidad Politecnica de Madrid
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+// JStack Neutron Module
+// ------------------
+
+JSTACK.Neutron = (function(JS, undefined) {
+    "use strict";
+    var params, check, configure, getnetworkslist, createnetwork, updatenetwork, getnetworkdetail, deletenetwork,
+    getsubnetslist, createsubnet, updatesubnet, getsubnetdetail, deletesubnet,
+    getportslist, createport, updateport, getportdetail, deleteport;
+
+    // This modules stores the `url`to which it will send every
+    // request. 
+    params = {
+        url: undefined,
+        state: undefined,
+        endpointType: "publicURL"
+    };
+
+    // Private functions
+    // -----------------
+
+    // Function `check` internally confirms that Keystone module is
+    // authenticated and it has the URL of the Glance service.
+    check = function() {
+        if (JS.Keystone !== undefined && JS.Keystone.params.currentstate === JS.Keystone.STATES.AUTHENTICATED) {
+            var service = JS.Keystone.getservice("network");
+            params.url = service.endpoints[0][params.endpointType];
+            return true;
+        }
+        return false;
+    };
+    // Public functions
+    // ----------------
+    //
+
+    // This function sets the endpoint type for making requests to Glance.
+    // It could take one of the following values:
+    // * "adminURL"
+    // * "internalURL"
+    // * "publicURL"
+    // You can use this function to change the default endpointURL, which is publicURL.
+    configure = function(endpointType) {
+        if (endpointType === "adminURL" || endpointType === "internalURL" || endpointType === "publicURL") {
+            params.endpointType = endpointType;
+        }
+    };
+
+    getnetworkslist = function(callback, error) {
+        var url, onOK, onError;
+        if (!check()) {
+            return;
+        }
+
+        url = params.url + 'v2.0/networks';
+
+        onOK = function(result) {
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.get(url, JS.Keystone.params.token, onOK, onError);
+    };
+
+    createnetwork = function(name, admin_state_up, shared, tenant_id, callback, error) {
+        var url, onOK, onError, data;
+        if (!check()) {
+            return;
+        }
+        url = params.url + 'v2.0/networks';
+
+        data = {
+            "network" : {
+            }
+        };
+
+        if (name !== undefined) {
+            data.network.name = name;
+        }
+
+        if (admin_state_up !== undefined) {
+            data.network.admin_state_up = admin_state_up;
+        }
+
+        if (shared !== undefined) {
+            data.network.shared = shared;
+        }
+
+        if (tenant_id !== undefined) {
+            data.network.tenant_id = tenant_id;
+        }
+
+        onOK = function(result) {
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.post(url, data, JS.Keystone.params.token, onOK, onError);
+    };
+
+    getnetworkdetail = function(network_id, callback, error) {
+        var url, onOK, onError;
+        if (!check()) {
+            return;
+        }
+        url = params.url + 'v2.0/networks/' + network_id;
+
+        onOK = function(result) {
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.get(url, JS.Keystone.params.token, onOK, onError);
+    };
+
+    updatenetwork = function(network_id, name, admin_state_up, callback, error) {
+        var url, onOK, onError, data;
+        if (!check()) {
+            return;
+        }
+        
+        url = params.url + 'v2.0/networks/' + network_id;
+
+        data = {
+            "network" : {
+            }
+        };
+
+        if (name !== undefined) {
+            data.network.name = name;
+        }
+
+        if (admin_state_up !== undefined) {
+            data.network.admin_state_up = admin_state_up;
+        }
+
+        onOK = function(result) {
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.put(url, data, JS.Keystone.params.token, onOK, onError);
+    };
+
+    deletenetwork = function(network_id, callback, error) {
+        var url, onOK, onError;
+        if (!check()) {
+            return;
+        }
+        url = params.url + 'v2.0/networks/' + network_id;
+
+        onOK = function(result) {
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.del(url, JS.Keystone.params.token, onOK, onError);
+    };
+
+    getsubnetslist = function(callback, error) {
+        var url, onOK, onError;
+        if (!check()) {
+            return;
+        }
+        
+        url = params.url + 'v2.0/subnets';
+
+        onOK = function(result) {
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.get(url, JS.Keystone.params.token, onOK, onError);
+    };
+
+    createsubnet = function(network_id, cidr, name, allocation_pools, tenant_id, gateway_ip, ip_version, enable_dhcp, dns_nameservers, host_routes, callback, error) {
+        var url, onOK, onError, data, i, start, end, allocation_pool, all_pools = [], dns_nameserver, dns_nservers = [], host_route, h_routes = [];
+        if (!check()) {
+            return;
+        }
+        
+        url = params.url + 'v2.0/subnets';
+
+        data = {
+            "subnet" : {
+                "network_id" : network_id,
+                "cidr" : cidr,
+                "ip_version" : 4
+            }
+        }
+
+        if (name !== undefined) {
+            data.subnet.name = name;
+        }
+
+        if (tenant_id !== undefined) {
+            data.subnet.tenant_id = tenant_id;
+        }
+
+        if (allocation_pools !== undefined) {
+            for (i in allocation_pools) {
+                if (allocation_pools[i] !== undefined) {
+                    allocation_pool = allocation_pools[i];
+                    all_pools.push(allocation_pool);
+                }
+            }
+            data.subnet.allocation_pools = all_pools;
+        }
+
+        if (gateway_ip !== undefined) {
+            data.subnet.gateway_ip = gateway_ip;
+        }
+
+        if (enable_dhcp !== undefined) {
+            data.subnet.enable_dhcp = enable_dhcp;
+        }
+
+        if (dns_nameservers !== undefined) {
+            for (i in dns_nameservers) {
+                if (dns_nameservers[i] !== undefined) {
+                    dns_nameserver = dns_nameservers[i];
+                    dns_nservers.push(dns_nameserver);
+                }
+            }
+            data.subnet.dns_nameservers = dns_nservers;
+        }
+
+        if (host_routes !== undefined) {
+            for (i in host_routes) {
+                if (host_routes[i] !== undefined) {
+                    host_route = host_routes[i];
+                    h_routes.push(host_route);
+                }
+            }
+            data.subnet.host_routes = h_routes;
+        }
+
+        JS.Comm.post(url, data, JS.Keystone.params.token, onOK, onError);
+    };
+
+    getsubnetdetail = function(subnet_id, callback, error) {
+        var url, onOK, onError;
+        if (!check()) {
+            return;
+        }
+
+        url = params.url + 'v2.0/subnets/' + subnet_id;
+
+        onOK = function(result) {
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.get(url, JS.Keystone.params.token, onOK, onError);
+    };
+
+    updatesubnet = function(subnet_id, name, gateway_ip, enable_dhcp, dns_nameservers, host_routes, callback, error) {
+        var url, onOK, onError, data, i, start, end, dns_nameserver, dns_nservers = [], host_route, h_routes = [];
+        if (!check()) {
+            return;
+        }
+        
+        url = params.url + 'v2.0/subnets/' + subnet_id;
+
+        data = {
+            "subnet" : {
+            }
+        }
+
+        if (name !== undefined) {
+            data.subnet.name = name;
+        }
+
+        if (gateway_ip !== undefined) {
+            data.subnet.gateway_ip = gateway_ip;
+        }
+
+        if (enable_dhcp !== undefined) {
+            data.subnet.enable_dhcp = enable_dhcp;
+        }
+
+        if (dns_nameservers !== undefined) {
+            for (i in dns_nameservers) {
+                if (dns_nameservers[i] !== undefined) {
+                    dns_nameserver = dns_nameservers[i];
+                    dns_nservers.push(dns_nameserver);
+                }
+            }
+            data.subnet.dns_nameservers = dns_nservers;
+        }
+
+        if (host_routes !== undefined) {
+            for (i in host_routes) {
+                if (host_routes[i] !== undefined) {
+                    host_route = host_routes[i];
+                    h_routes.push(host_route);
+                }
+            }
+            data.subnet.host_routes = h_routes;
+        }
+
+        onOK = function(result) {
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.put(url, data, JS.Keystone.params.token, onOK, onError);
+    };
+
+    deletesubnet = function(subnet_id, callback, error) {
+        var url, onOK, onError;
+        if (!check()) {
+            return;
+        }
+        
+        url = params.url + 'v2.0/subnets/' + subnet_id;
+
+        onOK = function(result) {
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.del(url, JS.Keystone.params.token, onOK, onError);
+    };
+
+    getportslist = function(callback, error) {
+        var url, onOK, onError;
+        if (!check()) {
+            return;
+        }
+        
+        url = params.url + 'v2.0/ports';
+
+        onOK = function(result) {   
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.get(url, JS.Keystone.params.token, onOK, onError);
+    };
+
+    createport = function(network_id, name, fixed_ips, security_groups, admin_state_up, status, tenant_id, mac_address, callback, error) {
+        var url, onOK, onError, data, groups = [], i, group, fixed_ip, fix_ips = [];
+        if (!check()) {
+            return;
+        }
+
+        url = params.url + 'v2.0/ports';
+
+        data = {
+            "port" : {
+                "network_id" : network_id
+            }
+        };
+
+        if (status !== undefined) {
+            data.port.status = status;
+        }
+
+        if (name !== undefined) {
+            data.port.name = name;
+        }
+
+        if (admin_state_up !== undefined) {
+            data.port.admin_state_up = admin_state_up;
+        }
+
+        if (tenant_id !== undefined) {
+            data.port.tenant_id = tenant_id;
+        }
+
+        if (mac_address !== undefined) {
+            data.port.mac_address = mac_address;
+        }
+
+        if (fixed_ips !== undefined) {
+            for (i in fixed_ips) {
+                if (fixed_ips[i] !== undefined) {
+                    fixed_ip = fixed_ips[i];
+                    fix_ips.push(fixed_ip);
+                }
+            }
+
+            data.port.fixed_ips = fix_ips;
+        }
+
+        if (security_groups !== undefined) {
+            for (i in security_groups) {
+                if (security_groups[i] !== undefined) {
+                    group = security_groups[i];
+                    groups.push(group);
+                }
+            }
+
+            data.port.security_groups = groups;
+        }
+
+        onOK = function(result) {
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.post(url, data, JS.Keystone.params.token, onOK, onError);
+    };
+
+    getportdetail = function(port_id, callback, error) {
+        var url, onOK, onError;
+        if (!check()) {
+            return;
+        }
+
+        url = params.url + 'v2.0/ports/' + port_id;
+
+        onOK = function(result) {   
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.get(url, JS.Keystone.params.token, onOK, onError);
+    };
+
+    updateport = function(port_id, name, fixed_ips, security_groups, admin_state_up, status, tenant_id, mac_address, callback, error) {
+        var url, onOK, onError, data, groups = [], i, group, fixed_ip, fix_ips = [];
+        if (!check()) {
+            return;
+        }
+        
+        url = params.url + 'v2.0/ports/' + port_id;
+
+        data = {
+            "port" : {
+            }
+        };
+
+        if (status !== undefined) {
+            data.port.status = status;
+        }   
+
+        if (name !== undefined) {
+            data.port.name = name;
+        }
+
+        if (admin_state_up !== undefined) {
+            data.port.admin_state_up = admin_state_up;
+        }
+
+        if (tenant_id !== undefined) {
+            data.port.tenant_id = tenant_id;
+        }
+
+        if (mac_address !== undefined) {
+            data.port.mac_address = mac_address;
+        }
+
+        if (fixed_ips !== undefined) {
+            for (i in fixed_ips) {
+                if (fixed_ips[i] !== undefined) {
+                    fixed_ip = fixed_ips[i];
+                    fix_ips.push(fixed_ip);
+                }
+            }
+
+            data.port.fixed_ips = fix_ips;
+        }
+
+        if (security_groups !== undefined) {
+            for (i in security_groups) {
+                if (security_groups[i] !== undefined) {
+                    group = security_groups[i];
+                    groups.push(group);
+                }
+            }
+
+            data.port.security_groups = groups;
+        }
+
+        onOK = function(result) {
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.put(url, data, JS.Keystone.params.token, onOK, onError);
+    };
+
+    deleteport = function(port_id, callback, error) {
+        var url, onOK, onError;
+        if (!check()) {
+            return;
+        }
+
+        url = params.url + 'v2.0/ports/' + port_id;
+
+        onOK = function(result) {
+            if (callback !== undefined) {
+                callback(result);
+            }
+        };
+        onError = function(message) {
+            if (error !== undefined) {
+                error(message);
+            }
+        };
+
+        JS.Comm.del(url, JS.Keystone.params.token, onOK, onError);
+    };
+
+    // Public Functions and Variables
+    // ------------------------------
+    // This is the list of available public functions and variables
+    return {
+        params : params,
+        // Functions:
+        configure: configure,
+        getnetworkslist : getnetworkslist,
+        getnetworkdetail : getnetworkdetail,
+        createnetwork : createnetwork,
+        updatenetwork : updatenetwork,
+        deletenetwork : deletenetwork,
+        getsubnetslist : getsubnetslist,
+        getsubnetdetail : getsubnetdetail,
+        createsubnet : createsubnet,
+        updatesubnet : updatesubnet,
+        deletesubnet : deletesubnet,
+        getportslist : getportslist,
+        getportdetail : getportdetail,
+        createport : createport,
+        updateport : updateport,
+        deleteport : deleteport
+    };
+
+}(JSTACK));
--- tools/compileDebug.sh
+++ tools/compileDebug.sh
@@ -8,3 +8,4 @@ cat ../src/JSTACK.Keystone.js >> $OUTPUT
 cat ../src/JSTACK.Nova.js >> $OUTPUT
 cat ../src/JSTACK.Nova.Volume.js >> $OUTPUT
 cat ../src/JSTACK.Glance.js >> $OUTPUT
+cat ../src/JSTACK.Neutron.js >> $OUTPUT
